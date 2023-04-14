5e98f89023fed5ca55cd39fcd41b840fa11d5bb2
SayedM00 <elsayedmoharam000@gmail.com>
2023-04-14 20:05:22 +0200

    The First JS Porject

---
+++ js/app.js
@@ -0,0 +1,158 @@
+// Variables
+let colors = document.querySelectorAll(".colors li");
+let links = document.querySelectorAll(".landing-page .head-area .links li a")
+let body_style = document.documentElement.style;
+let bg_checkbox = document.querySelector(".bg-checkbox");
+let nav_checkbox = document.querySelector(".nav-checkbox");
+let lb_control;
+let skillsBox = document.querySelector(".skills-section");
+let skills = document.querySelectorAll(".skills-section .skills li span");
+let rightClick = document.querySelector(".right");
+let leftClick = document.querySelector(".left");
+let imgs = document.querySelectorAll(".gallery-section .imgs img");
+let navBullets = document.querySelectorAll(".nav-bullets .bullet");
+
+if (localStorage.getItem("color_option") !== null) {
+    // Save The Choosen Color
+    body_style.setProperty("--main--color", localStorage.getItem("color_option"))
+    // Save Class Active On The Choosen Color
+    for(let i = 0; i < colors.length; i++) {
+        if (localStorage.getItem("color_option") == colors[i].dataset.color) {
+            colors.forEach((ele) => {
+                ele.classList.remove("active")
+            });
+            colors[i].classList.add("active")
+        }
+    }
+}
+if (localStorage.getItem("image_number") != null) {
+    // Save The Choosen Image
+    document.getElementById("landing").style.backgroundImage = `url('../imgs/landing-page\ ${localStorage.getItem("image_number")}')`
+}
+
+// Setting Box And Gear
+document.querySelector(".gear-p").onclick = ()=> {
+    document.querySelector(".setting-box").classList.toggle("open");
+    document.querySelector(".gear-c").classList.toggle("fa-spin")
+
+}
+// Control Colors
+colors.forEach((ele) => {
+    ele.onclick = function() {
+        // Remove Active Class From All Colors
+        colors.forEach((ele) => {
+            ele.classList.remove("active")
+        })
+        // Add Active Class On The Choosen Color
+        this.classList.add("active")
+        // Change Main Color's Body Style
+        body_style.setProperty("--main--color", this.dataset.color)
+        // Save The Choosen Color in Local Storage
+        localStorage.setItem("color_option", this.dataset.color)
+    }
+})
+// Links Switch 
+document.querySelector(".bars").addEventListener(("click"), () => {
+    document.querySelector(".links").classList.toggle("links-open")
+})
+// Check Background Switch's value
+if (localStorage.getItem("backgroundSwitch") != null) {
+    if (localStorage.getItem("backgroundSwitch") == "true") {
+        bg_checkbox.click();
+        switchBg()
+    }
+}
+// Landing Page Switch 
+document.querySelector(".bg-toggle-switch").onclick = function () {
+    if (bg_checkbox.checked == true) {
+        localStorage.setItem("backgroundSwitch", "false")
+        clearInterval(lb_control)
+    } else {
+        localStorage.setItem("backgroundSwitch", "true")
+        switchBg()
+    }
+}
+// Switch Function
+function switchBg() {
+    let number; 
+    lb_control = setInterval(() => {
+            number= `0${Math.ceil(Math.random() * 5)}.jpg`;
+            document.getElementById("landing").style.backgroundImage = `url('../imgs/landing-page\ ${number}')`
+            localStorage.setItem("image_number", number)
+        }, 5000);
+    }
+if (localStorage.getItem("navSwitch") != null) {
+    if (localStorage.getItem("navSwitch") === 'true') {
+        document.querySelector(".nav-bullets").style.display = "block";
+        nav_checkbox.click()
+    } else {
+        document.querySelector(".nav-bullets").style.display = "none"
+    }
+}
+// Nav Bullents Switch 
+document.querySelector(".nav-toggle-switch").onclick = function () {
+    if (nav_checkbox.checked == true) {
+        localStorage.setItem("navSwitch", "false")
+        document.querySelector(".nav-bullets").style.display = "none"
+    } else {
+        localStorage.setItem("navSwitch", "true")
+        document.querySelector(".nav-bullets").style.display = "block";
+    }
+}
+// Skills Progress
+window.onscroll = () => {
+    let skillsOfSet = skillsBox.offsetTop;
+    let skillsInnerHeight = skillsBox.offsetHeight;
+    let windowHeight = this.innerHeight;
+    let windowScroll = this.scrollY;
+    if (windowScroll > (skillsOfSet + skillsInnerHeight - windowHeight)) {
+        skills.forEach((span) => {
+            span.style.width = span.dataset.progress
+            // console.log(li.getAttribute("data-progress"))
+        })
+    } else {
+        skills.forEach((span) => {
+            span.style.width = "0"
+        })
+    }
+}
+
+// Gallery Section 
+let x = Number(false)
+
+rightClick.onclick = function () {
+    imgs.forEach((ele) => ele.classList.remove("gallery-effect"))
+
+    if (x === imgs.length - 1) {
+        x = Number(false);
+        imgs[Number(false)].classList.add("gallery-effect")
+
+    } else {
+        x += 1
+        imgs[x].classList.add("gallery-effect")
+    }
+} 
+
+leftClick.onclick = function() {
+    imgs.forEach((ele) => ele.classList.remove("gallery-effect"))
+    if (x === Number(false)) {
+        x = imgs.length - 1
+        imgs[imgs.length - 1].classList.add("gallery-effect")
+    } else {
+        x -= 1
+        imgs[x].classList.add("gallery-effect");
+    }
+}
+
+function windowScroll(elements) {
+    elements.forEach((ele) => {
+        ele.addEventListener("click", function(e) {
+            window.scrollTo({
+                top : document.querySelector(e.target.dataset.section).offsetTop,
+                behavior : "smooth"
+            })
+        })
+    })
+}
+windowScroll(links)
+windowScroll(navBullets)
